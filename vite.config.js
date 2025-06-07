7cfc202fe5189a9e8642eb5a8f92a6049fcd39d8
sumitra773 <ksumitra773@gmail.com>
2025-06-07 16:31:41 +0530

    rahul

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/multistep-form",
+  base: '/multistep-form/', // 👈 important if your Render URL is https://.../multistep-form
   plugins: [react()],
-});
+})
